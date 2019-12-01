6f85dcc3b60881aab9aa89c6ad7a665d1e34e917
nav <navsandhu343@gmail.com>
2019-12-01 15:00:43 -0500

    launching screen

---
+++ app/src/main/java/com/example/capsproject/launchActivity.java
@@ -0,0 +1,35 @@
+package com.example.capsproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class launchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        final Context context;
+        context = this;
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (Exception e){
+                    Log.e("Launch","Thread wait failed");
+
+                }finally {
+                    finish();
+                    Intent loginintent = new Intent(context,MainActivity.class);
+                    startActivity(loginintent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+}
